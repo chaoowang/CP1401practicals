556809ba1f863448538b853d41e8e9c2e0f63b81
wangchaohsuan <chaohsuaan@gmail.com>
2020-11-27 10:43:30 +0800

    Refactor temperatures.py to use functions

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -1,9 +1,22 @@
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = celsius_to_fahrenheit()
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            celsius = fahrenheit_to_celsius()
+            print("Result:{:.2f} C".format(celsius))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
 
 def celsius_to_fahrenheit():
     celsius = float(input("Celsius: "))
@@ -16,16 +29,5 @@ def fahrenheit_to_celsius():
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
 
-
-while choice != "Q":
-    if choice == "C":
-        fahrenheit = celsius_to_fahrenheit()
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        celsius = fahrenheit_to_celsius()
-        print("Result:{:.2f} C".format(celsius))
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+if __name__ == '__main__':
+    main()
