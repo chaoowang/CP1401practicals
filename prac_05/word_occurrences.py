728ee6176e639b96faf16ca571ef89eefbcf4a3c
wangchaohsuan <chaohsuaan@gmail.com>
2020-12-17 14:04:32 +0800

    reformat code

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,13 +1,13 @@
 def main():
-    word_to_count={}
-    text=input("Text:")
-    words= text.split()
+    word_to_count = {}
+    text = input("Text:")
+    words = text.split()
     for word in words:
-        times= word_to_count.get(word,0)
-        word_to_count[word]=times+1
-    words=list(word_to_count.keys())
+        times = word_to_count.get(word, 0)
+        word_to_count[word] = times + 1
+    words = list(word_to_count.keys())
     words.sort()
-    max_len=max(len(word) for word in words)
+    max_len = max(len(word) for word in words)
     for word in words:
         print("{:{}} : {}".format(word, max_len, word_to_count[word]))
 
