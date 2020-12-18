644b1d757f4e6eaf79671e637c409740108ffcd1
wangchaohsuan <chaohsuaan@gmail.com>
2020-12-18 19:03:33 +0800

    fix type error

--- prac_06/Guitar.py
+++ prac_06/Guitar.py
@@ -12,7 +12,7 @@ class Guitar:
         return age
 
     def is_vintage(self):
-        age = self.get_age(self)
+        age = self.get_age()
         if age>=50:
             return True
         else:
