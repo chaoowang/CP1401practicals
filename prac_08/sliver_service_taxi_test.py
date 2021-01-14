f0b2ba19ba43501ab7b93d1d8a26a58e2dda4fc8
wangchaohsuan <chaohsuaan@gmail.com>
2021-01-14 23:22:01 +0800

    add fare

--- prac_08/sliver_service_taxi_test.py
+++ prac_08/sliver_service_taxi_test.py
@@ -1,10 +1,13 @@
 from prac_08.sliver_service_taxi import SliverServiceTaxi
 
-def main():
 
-    test_car=SliverServiceTaxi("fancy car",20,2)
+def main():
+    test_car = SliverServiceTaxi("fancy car", 20, 2)
     test_car.drive(18)
     print(test_car)
+    current_fare = test_car.get_fare()
+    print("current fare: ${}".format(current_fare))
+
 
 if __name__ == '__main__':
     main()
