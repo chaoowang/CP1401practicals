adfb7879bb4faa6ec0aef4da5475aa1db8e6ff5a
wangchaohsuan <chaohsuaan@gmail.com>
2021-01-14 10:56:29 +0800

    fix error

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -3,29 +3,31 @@ from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.properties import StringProperty
 
-class dynamic_label(App):
 
-    status_text=StringProperty()
+class dynamic_label(App):
+    status_text = StringProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.names=["Ben","Ray","Janice","Carol"]
+        self.names = ["Ben", "Ray", "Janice", "Carol"]
 
     def build(self):
-        self.title="dynamic labels"
-        self.root=Builder.load_file("dynamic_label.kv")
+        self.title = "dynamic labels"
+        self.root = Builder.load_file("dynamic_label.kv")
         self.create_widget()
         return self.root
 
     def create_widget(self):
         for name in self.names:
-            temp_label=Label(text=name, id=name)
+            temp_label = Label()
+            temp_label.text = name
+            temp_label.id = name
             temp_label.bind(on_press=self.press_entry)
             self.root.ids.entries_box.add_widget(temp_label)
 
     def press_entry(self, instance):
-        name=instance.id
-        self.status_text="".format(name)
+        name = instance.id
+        self.status_text = "".format(name)
 
 
 dynamic_label().run()
