e68218b9ae2a163d728414e6cf26ddf3f534e7ef
wangchaohsuan <chaohsuaan@gmail.com>
2021-01-14 15:33:48 +0800

    test taxi

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,18 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi.drive(40)
+    print(new_taxi)
+    current_fare = new_taxi.get_fare()
+    print("current fare: {}".format(current_fare))
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    print(new_taxi)
+    current_fare = new_taxi.get_fare()
+    print("current fare: {}".format(current_fare))
+
+
+if __name__ == '__main__':
+    main()
