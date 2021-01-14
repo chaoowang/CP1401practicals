15d10aa6a2c886c1a39afae793333e23debd48d1
wangchaohsuan <chaohsuaan@gmail.com>
2021-01-14 22:25:07 +0800

    add class SliverServiceTaxi

---
+++ prac_08/SliverServiceTaxi.py
@@ -0,0 +1,16 @@
+from prac_08.taxi import Taxi
+
+
+class SliverServiceTaxi(Taxi):
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * self.fanciness
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        return "{} plus flagfall of ${}".format(super().__str__(),self.flagfall)
