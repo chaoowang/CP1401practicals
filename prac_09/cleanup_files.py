0979c864745656348ae72c9da88891ba3b61abac
wangchaohsuan <chaohsuaan@gmail.com>
2021-01-27 14:39:41 +0800

    add fix_filename

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -32,9 +32,28 @@ def main():
         print("Renaming {} to {}".format(filename, new_name))
 
 
-def get_fixed_filename(filename):
+def fix_filename_step_one(filename):
     """Return a 'fixed' version of filename."""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+
+    for i, char in enumerate(new_name):
+        try:
+            if new_name[i].isalpha() and new_name[i].islower():
+                if new_name[i + 1].isalpha() and new_name[i + 1].isupper():
+                    new_name = new_name[:i + 1] + "_" + new_name[i + 1:]
+        except IndexError:
+            pass
+    return new_name
+
+
+def fix_filename_step_two(filename):
+    new_name = filename
+    for i, char in enumerate(new_name):
+        try:
+            if new_name[i] == "_" and new_name[i + 1].islower():
+                new_name = new_name[:i + 1] + new_name[i + 1].upper() + new_name[i + 2:]
+        except IndexError:
+            pass
     return new_name
 
 
@@ -42,16 +61,16 @@ def demo_walk():
     """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
+        # print("Directory:", directory_name)
+        # print("\tcontains subdirectories:", subdirectories)
+        # print("\tand files:", filenames)
+        # print("(Current working directory is: {})".format(os.getcwd()))
 
         for filename in filenames:
             old_name = os.path.join(directory_name, filename)
-            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            fix_name = os.path.join(directory_name, fix_filename_step_one(filename))
+            new_name = os.path.join(directory_name, fix_filename_step_two(fix_name))
             os.renames(old_name, new_name)
-
 
 
 demo_walk()
